32f621387709974f0a1b1689509f6dfd979ed71a
CHEONG JUN KAI <19043996@myrp.edu.sg>
2020-05-26 21:44:51 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19043996/demoriddle/AnswerActivity1.java
@@ -0,0 +1,28 @@
+package sg.edu.rp.c346.id19043996.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class AnswerActivity1 extends AppCompatActivity {
+
+    TextView tvAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer1);
+
+        Log.d("AnswerActivity1", "onCreate() called.");
+
+        tvAnswer = findViewById(R.id.textView);
+        //tvAnswer.setText("In Second Activity");
+
+        Intent intentReceived = getIntent();
+        String questionsSelected = intentReceived.getStringExtra( "Question");
+        tvAnswer.setText(questionsSelected + " answer is: Queue");
+    }
+}
